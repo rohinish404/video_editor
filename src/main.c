9b23456b8c62680f0e3b709fb0f7e7009fdd0a4d
Rohinish <singhrohinish09@gmail.com>
2024-06-27 00:39:05 +0530

    render video

--- src/main.c
+++ src/main.c
@@ -2,6 +2,32 @@
 #include <libavcodec/avcodec.h>
 #include <libavformat/avformat.h>
 #include <libswscale/swscale.h>
+
+
+int decode_video(AVCodecContext *avctx, AVFrame *frame, int *got_frame, AVPacket *pkt)
+{
+  int ret;
+
+  *got_frame = 0;
+
+  if (pkt) {
+    ret = avcodec_send_packet(avctx, pkt);
+    if (ret < 0) {
+      return ret;
+    }
+  }
+
+  ret = avcodec_receive_frame(avctx, frame);
+  if (ret < 0 && ret != AVERROR(EAGAIN) && ret != AVERROR_EOF) {
+    return ret;
+  }
+  if (ret >= 0) {
+    *got_frame = 1;
+  }
+
+  return 0;
+}
+
 int main(void){
 
   const int screenWidth = 800;
@@ -9,24 +35,24 @@ int main(void){
 
   InitWindow(screenWidth, screenHeight,"video editor");
   AVFormatContext *formatContext = NULL;
-    if (avformat_open_input(&formatContext, "assets/sunset.mp4", NULL, NULL) != 0) {
-      printf("Couldn't open video file\n");
-      return -1;
+  if (avformat_open_input(&formatContext, "assets/sunset.mp4", NULL, NULL) != 0) {
+    printf("Couldn't open video file\n");
+    return -1;
   }
 
   int videoStream = -1;
   for (int i = 0; i < formatContext->nb_streams; i++) {
     if (formatContext->streams[i]->codecpar->codec_type== AVMEDIA_TYPE_VIDEO) {
-        videoStream = i;
-        printf("%d",videoStream);
-        break;
-      }
+      videoStream = i;
+      printf("%d",videoStream);
+      break;
+    }
   }
   if (videoStream == -1) {
     printf("Couldn't find a video stream\n");
     return -1;
   }
-  
+
   AVCodecContext *codecContext = avcodec_alloc_context3(NULL);
   if (!codecContext) {
     fprintf(stderr, "Failed to allocate codec context\n");
@@ -38,13 +64,13 @@ int main(void){
     return -1;
   }
   const AVCodec *codec = avcodec_find_decoder(codecContext->codec_id);
-    if (codec == NULL) {
-        printf("Unsupported codec\n");
-        return -1;
+  if (codec == NULL) {
+    printf("Unsupported codec\n");
+    return -1;
   }
   if (avcodec_open2(codecContext, codec, NULL) < 0) {
-        printf("Couldn't open codec\n");
-        return -1;
+    printf("Couldn't open codec\n");
+    return -1;
   }
   AVFrame *frame = av_frame_alloc();
   AVPacket packet;
@@ -56,36 +82,50 @@ int main(void){
 
   while (!WindowShouldClose())
   {
-    //if (av_read_frame(formatContext, &packet) >= 0) {
-    //        if (packet.stream_index == videoStream) {
-    //            // Decode video frame
-    //            int frameFinished;
-    //            avcodec_decode_video2(codecContext, frame, &frameFinished, &packet);
+    if (av_read_frame(formatContext, &packet) >= 0) {
+      if (packet.stream_index == videoStream) {
+        int got_frame = 0;
+        int ret = decode_video(codecContext, frame, &got_frame, &packet);
 
-    //            if (frameFinished) {
-    //                // Convert the image from its native format to RGB
-    //                struct SwsContext *sws_ctx = sws_getContext(
-    //                    codecContext->width, codecContext->height, codecContext->pix_fmt,
-    //                    codecContext->width, codecContext->height, AV_PIX_FMT_RGB24,
-    //                    SWS_BILINEAR, NULL, NULL, NULL
-    //                );
+        if (ret < 0) {
+          printf("Error decoding video\n");
+          break;
+        }
 
-    //                // Update the image data
-    //                UpdateImageRaw(&image, frame->data[0]);
+        if (got_frame) {
 
-    //                // Update the texture
-    //                UpdateTexture(texture, image.data);
+          struct SwsContext *sws_ctx = sws_getContext(
+            codecContext->width, codecContext->height, codecContext->pix_fmt,
+            codecContext->width, codecContext->height, AV_PIX_FMT_RGB24,
+            SWS_BILINEAR, NULL, NULL, NULL
+          );
+          uint8_t *buffer = (uint8_t *)malloc(codecContext->width * codecContext->height * 3);
+          int linesize[1] = { 3 * codecContext->width };
 
-    //                sws_freeContext(sws_ctx);
-    //            }
-    //        }
-    //        av_packet_unref(&packet);
-    //    }
-      BeginDrawing();
-        ClearBackground(RAYWHITE);
-       // DrawTexture(texture, 0, 0, WHITE);
-      DrawCircle(screenWidth/5, 120,30, DARKBLUE);
+          sws_scale(sws_ctx, (const uint8_t * const*)frame->data, frame->linesize, 0,
+                    codecContext->height, &buffer, linesize);
+          for (int y = 0; y < codecContext->height; y++) {
+            for (int x = 0; x < codecContext->width; x++) {
+              Color color = {
+                buffer[(y * codecContext->width + x) * 3],
+                buffer[(y * codecContext->width + x) * 3 + 1],
+                buffer[(y * codecContext->width + x) * 3 + 2],
+                255
+              };
+              ImageDrawPixel(&image, x, y, color);
+            }
+          }
 
+          UpdateTexture(texture, image.data);
+          free(buffer);
+          sws_freeContext(sws_ctx);
+        }
+      }
+      av_packet_unref(&packet);
+    }
+    BeginDrawing();
+    ClearBackground(RAYWHITE);
+    DrawTexture(texture, 0, 0, WHITE);
     EndDrawing();
 
 
@@ -93,10 +133,10 @@ int main(void){
   UnloadTexture(texture);
   UnloadImage(image);
   av_frame_free(&frame);
-  avcodec_close(codecContext);
+  avcodec_free_context(&codecContext); 
   avformat_close_input(&formatContext);
   CloseWindow();
-  
+
   return 0;
 
 
